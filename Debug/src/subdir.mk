42fa387857b339a033d335bf47f619d48e8a447d
Ross Harrison <rtharrison86@gmail.com>
2018-10-24 23:20:13 -0400

    Runs in N-Sight

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../src/N-Body.cu 
+
+OBJS += \
+./src/N-Body.o 
+
+CU_DEPS += \
+./src/N-Body.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-9.1/bin/nvcc -G -g -O0 -ccbin /usr/bin/g++-6 -gencode arch=compute_61,code=sm_61  -odir "src" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-9.1/bin/nvcc -G -g -O0 -ccbin /usr/bin/g++-6 --compile --relocatable-device-code=false -gencode arch=compute_61,code=compute_61 -gencode arch=compute_61,code=sm_61  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
