760aa3ac2738e66f950dec2f5717140273fc0d9f
Ross Harrison <rtharrison86@gmail.com>
2018-10-28 00:27:27 -0400

    Calculate Initial Forces

--- src/N-Body.cu
+++ src/N-Body.cu
@@ -4,216 +4,133 @@
 #include <thrust/iterator/counting_iterator.h>
 #include <iostream>
 #include <ctime>
-//#include <opencv2/opencv.hpp>
+#include <cmath>
 
-const int PIXEL_RGBA_RATIO = 3;
+const float EPS2 = M_E * M_E;
 
 /**
  * Struct representing a body
  */
 struct Body {
-  float x, y, vx, vy;
+	float3 position, velocity;
 
-  __host__ __device__
-    Body (float _x, float _y, float _vx, float _vy):
-      x(_x), y(_y), vx(_vx), vy(_vy) {};
-  __host__ __device__
-    Body (): x(0.), y(0.), vx(0.), vy(0.) {};
+	__host__ __device__ Body(float3 _position, float3 _velocity):
+			position(_position), velocity(_velocity) {};
+
+	__host__ __device__ Body() : position(float3{0., 0., 0.}), velocity(float3{0., 0., 0.}) {};
 };
 
 /**
  * Image Coordinates
  */
 struct ImageCoord {
-  int x, y, imageDim;
+	int x, y, imageDim;
 
-  __host__ __device__
-    // create coordinate from components
-    ImageCoord (int _x, int _y, int _imageDim): x(_x), y(_y), imageDim(_imageDim) {};
+	__host__ __device__
+	// create coordinate from components
+	ImageCoord(int _x, int _y, int _imageDim) :
+			x(_x), y(_y), imageDim(_imageDim) {
+	};
 
-  __host__ __device__
-    // default initialization
-    ImageCoord (): x(0), y(0), imageDim(0) {};
+	__host__ __device__
+	// default initialization
+	ImageCoord() :
+			x(0), y(0), imageDim(0) {
+	};
 
-  __host__ __device__
-    // create image coordinate from a body's position
-    ImageCoord (const Body &body, int _imageDim):  imageDim(_imageDim) {
-      auto halfImageDim = imageDim / 2;
-      x = (body.x + 1.) * halfImageDim;
-      y = (body.y + 1.) * halfImageDim;
-    };
+	__host__ __device__
+	// create image coordinate from a body's position
+	ImageCoord(const Body &body, int _imageDim) :
+			imageDim(_imageDim) {
+		auto halfImageDim = imageDim / 2;
+		x = (body.position.x + 1.) * halfImageDim;
+		y = (body.position.y + 1.) * halfImageDim;
+	};
 
-  __host__ __device__
-    int toOffset() {
-      return x + y * imageDim;
-    };
+	__host__ __device__
+	int toOffset() {
+		return x + y * imageDim;
+	};
 };
 
 /**
  * Program to initialize bodies with random values between -1,1
  */
-struct initRandomPrg
-{
-  float minValue, maxValue;
-  int seed;
+struct initRandomPrg {
+	float minValue, maxValue;
+	int seed;
 
-  __host__ __device__
-    initRandomPrg(int _seed=0, float _mnV=-1.f, float _mxV=1.f):
-      seed(_seed), minValue(_mnV), maxValue(_mxV) {};
+	__host__ __device__ initRandomPrg(int _seed = 0, float _mnV = -1.f,
+			float _mxV = 1.f) :
+			seed(_seed), minValue(_mnV), maxValue(_mxV) {
+	};
 
-  __host__ __device__
-    Body operator()(const unsigned int idx) const
-    {
-      thrust::default_random_engine rng(seed);
-      thrust::uniform_real_distribution<float> dist(minValue, maxValue);
-      rng.discard(idx);
+	__host__  __device__ Body operator()(const unsigned int idx) const {
+		thrust::default_random_engine rng(seed);
+		thrust::uniform_real_distribution<float> dist(minValue, maxValue);
+		rng.discard(idx);
 
-      return Body{
-        dist(rng), dist(rng),
-          dist(rng), dist(rng)
-      };
-    }
+		return Body {float3{dist(rng), dist(rng), dist(rng)},
+					float3{dist(rng), dist(rng),dist(rng)}};
+	}
 };
 
+struct calculateAcceleration {
+	const Body *bodies;
+	const int bodyCount;
 
-struct mapBodyToPixelCounts
-{
-  int *pixelCounts;
-  const int imageDim;
+	__host__ __device__ calculateAcceleration(
+			const Body *_bodies, int _bodyCount):
+			bodies(_bodies), bodyCount(_bodyCount) {};
 
-  __host__ __device__
-    mapBodyToPixelCounts(
-        int _imageDim,
-        int *_pixelCounts
-        ): imageDim(_imageDim), pixelCounts(_pixelCounts) {};
+	__host__  __device__ float3 operator()(const Body &body) {
+		float3 result { 0., 0., 0. };
 
-  __device__
-    void operator()(const Body &body) const
-    {
-      auto offset = ImageCoord(body, imageDim).toOffset();
-      atomicAdd(&pixelCounts[offset], 1);
-    }
-};
+		for (int i = 0; i < bodyCount; i++) {
+			auto otherBody = bodies[i];
+			if (&otherBody == &body) continue;
 
-struct mapPixelCountToRGBA
-{
-  const int BODY_COUNT_GRAYSCALE_RATIO = 1; // like 12 points in white
-  const int *pixelCount_ptr;
-  unsigned char *image_ptr;
-  __host__ __device__
-  mapPixelCountToRGBA(
-    int *_pixelCounts,
-    unsigned char *_image
-  ): pixelCount_ptr(_pixelCounts), image_ptr(_image) {};
 
-  __host__ __device__
-  void operator()(const unsigned int idx) {
-    auto count = pixelCount_ptr[idx];
-    auto grayValue = min(BODY_COUNT_GRAYSCALE_RATIO * count, 127);
-    // assign rgba values
-    auto baseIdx = idx * PIXEL_RGBA_RATIO;
-    image_ptr[baseIdx] = grayValue;
-    image_ptr[baseIdx + 1] = grayValue;
-    image_ptr[baseIdx + 2] = grayValue;
-  }
+			auto position = otherBody.position;
+			auto diffX = body.position.x - position.x;
+			auto diffY = body.position.y - position.y;
+			auto diffZ = body.position.z - position.z;
+			auto distSqr = diffX * diffX +
+					diffY * diffY +
+					diffZ * diffZ + EPS2;
+			auto distSixth = distSqr * distSqr * distSqr;
+			auto invDistCube = 1.0f / sqrtf(distSixth);
+
+			result.x += invDistCube * diffX;
+			result.y += invDistCube * diffY;
+			result.z += invDistCube * diffZ;
+		}
+
+		return result;
+	}
 };
 
 int main(int argc, char **argv) {
-//  if (argc != 2) {
-//    std::cerr << "Usage: n-body [output file]" << std::endl;
-//    return 1;
-//  }
+	int const BODY_COUNT = 10e6;
+	int const IMAGE_DIM = 400;
 
-  int const BODY_COUNT = 10e6;
-  int const IMAGE_DIM = 400;
-  int const PIXEL_COUNT = IMAGE_DIM * IMAGE_DIM;
-  int const RGBA_IMAGE_SIZE = PIXEL_COUNT * PIXEL_RGBA_RATIO; // image has 4 values per pixels
+	// initialize bodies
+	auto bodies = thrust::device_vector<Body>(BODY_COUNT);
+	auto bodies_ptr = thrust::raw_pointer_cast(bodies.data());
+	auto index_sequence_begin = thrust::counting_iterator<unsigned int>(0);
 
-  // initilize bodies
-  auto bodies = thrust::device_vector<Body>(BODY_COUNT);
-  auto index_sequence_begin = thrust::counting_iterator<unsigned int>(0);
+	thrust::transform(index_sequence_begin, index_sequence_begin + BODY_COUNT,
+			bodies.begin(), initRandomPrg(std::time(0)));
 
-  thrust::transform(
-    index_sequence_begin,
-    index_sequence_begin + BODY_COUNT,
-    bodies.begin(),
-    initRandomPrg(std::time(0))
-  );
+	std::cout << "Initialized Bodies" << "\n\n";
 
-  std::cout << "Initialized Bodies" << "\n\n";
+	// calculate forces
+	auto forces = thrust::device_vector<float3>(BODY_COUNT);
+	thrust::transform(
+			bodies.begin(), bodies.end(), forces.begin(),
+			calculateAcceleration(bodies_ptr, bodies.size()));
 
-  // initialize pixel counts
-  auto pixelCounts = thrust::device_vector<int>(PIXEL_COUNT);
-  auto pixelCount_ptr = thrust::raw_pointer_cast(pixelCounts.data());
-  thrust::fill(pixelCounts.begin(), pixelCounts.end(), 0);
+	std::cout << "Calculate Initial Forces" << "\n\n";
 
-  std::cout << "Initialized Pxels" << "\n\n";
-
-  // get pixel counts
-  thrust::for_each(
-    bodies.begin(), bodies.end(),
-    mapBodyToPixelCounts(IMAGE_DIM, pixelCount_ptr)
-  );
-
-  std::cout << "Mapped Points to Pixels" << "\n\n";
-
-  auto deviceImage = thrust::device_vector<unsigned char>(RGBA_IMAGE_SIZE);
-  auto deviceImage_ptr = thrust::raw_pointer_cast(deviceImage.data());
-  thrust::fill(deviceImage.begin(), deviceImage.end(), 0);
-
-  thrust::for_each(
-    index_sequence_begin,
-    index_sequence_begin + PIXEL_COUNT,
-    mapPixelCountToRGBA(pixelCount_ptr, deviceImage_ptr)
-  );
-
-  std::cout << "Initialized Image Pixels" << "\n\n";
-
-  for (auto i = 0; i < 10; i++) {
-    Body b = bodies[i];
-    auto bc = ImageCoord(b, IMAGE_DIM);
-    auto offset = ImageCoord(b, IMAGE_DIM).toOffset();
-    auto baseIdx = i * PIXEL_RGBA_RATIO;
-
-    auto red = (int)deviceImage[baseIdx];
-    auto blue = (int)deviceImage[baseIdx + 1];
-    auto green = (int)deviceImage[baseIdx + 2];
-
-    std::cout << b.x << '\t' << b.y << '\t';
-    std::cout << offset << ' ' <<  bc.x << ',' << bc.y << "\t\t";
-    std::cout << red << ", " <<  blue << ", " << green;
-    std::cout << '\n';
-  }
-
-  auto max = thrust::reduce(
-      pixelCounts.begin(), pixelCounts.end(),
-      0, thrust::maximum<int>()
-  );
-
-  auto min = thrust::reduce(
-      pixelCounts.begin(), pixelCounts.end(),
-      0, thrust::minimum<int>()
-  );
-
-  auto minColor = thrust::reduce(
-      deviceImage.begin(), deviceImage.end(),
-      0, thrust::minimum<int>()
-  );
-
-  std::cout << "Max Pixel Count: \t" << max << '\n';
-  std::cout << "Min Pixel Count: \t" << min << '\n';
-  std::cout << "Min Pixel Value: \t" << minColor << '\n';
-  std::cout << "Body Count: \t\t" << BODY_COUNT << '\n';
-
-  // copy image to host
-  auto hostImage = thrust::host_vector<unsigned char>(RGBA_IMAGE_SIZE);
-  thrust::copy(deviceImage.begin(), deviceImage.end(), hostImage.begin());
-
-  // copy host image to matrix
-//  auto hostImage_ptr = thrust::raw_pointer_cast(hostImage.data());
-//  cv::Mat imageMat(IMAGE_DIM, IMAGE_DIM, CV_8UC3);
-//  memcpy(imageMat.data, hostImage_ptr, sizeof(unsigned char) * RGBA_IMAGE_SIZE);
-//
-//  cv::imwrite("/home/rharriso/Desktop/Test.png", imageMat);
-  return 0;
+	return 0;
 }
